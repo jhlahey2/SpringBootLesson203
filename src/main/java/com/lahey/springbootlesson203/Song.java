971fc6a5bf21bbd7ba6b4b3815f1af8db0e5c26d
GBTC440009UR <GBTC440009UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-23 16:26:54 -0400

    first commit

---
+++ src/main/java/com/lahey/springbootlesson203/Song.java
@@ -0,0 +1,66 @@
+package com.lahey.springbootlesson203;
+
+public class Song {
+
+    //class variables
+    private long id;
+    private String name;
+    private String artist;
+    private String album;
+    private int rating;
+    private int year;
+
+    //default constructor
+    public Song(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}//end public class Song
---
+++ src/main/java/com/lahey/springbootlesson203/Springbootlesson203Application.java
@@ -0,0 +1,12 @@
+package com.lahey.springbootlesson203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootlesson203Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootlesson203Application.class, args);
+	}
+}
